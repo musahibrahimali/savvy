9d49ed8aba15e00e19458eb9abddc13fbd304807
Musah Ibrahim Ali <musahibrahimali@gmail.com>
2022-02-27 03:17:29 +0000

    adding bottom navigation images

--- lib/ui/views/components/custom_bottom_bar.dart
+++ lib/ui/views/components/custom_bottom_bar.dart
@@ -31,16 +31,20 @@ class CustomBottomNavBar extends StatelessWidget {
           fontWeight: FontWeight.w700,
           color: Colors.teal,
         ),
-        items: const [
+        items: [
           BottomNavigationBarItem(
-            icon: Icon(
-              Icons.store_mall_directory_sharp,
+            icon: Image.asset(
+              'assets/images/whatsapp.png',
+              width: 30.0,
+              height: 30.0,
             ),
             label: "WhatsApp",
           ),
           BottomNavigationBarItem(
-            icon: Icon(
-              Icons.store_mall_directory_outlined,
+            icon: Image.asset(
+              'assets/images/whatsapp_business.png',
+              width: 30.0,
+              height: 30.0,
             ),
             label: "Business",
           ),
